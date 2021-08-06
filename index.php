a1a79e1cdfebf9b45049e9417cd4f6636051c160
number <number@mashine.local>
2021-08-06 23:44:19 +0200

    index cleanup

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
 <?php 
-$data = Timber::get_context();
-
-$data['top_menu'] = new TimberMenu('top_menu_'.ICL_LANGUAGE_CODE);
-$data['languages'] = apply_filters( 'wpml_active_languages', NULL, 'orderby=code&order=asc&skip_missing=0' );
-$data['current_language'] = ICL_LANGUAGE_CODE;
-
-if(is_front_page()){
-	$data['post'] = new TimberPost();
-	Timber::render('frontpage.twig', $data);
-}elseif(is_archive()){
-	$data['posts'] = Timber::get_posts();
-	Timber::render('archive.twig', $data);		
-}else{
-	$data['post'] = new TimberPost();
-	Timber::render('page.twig', $data);		
-}
